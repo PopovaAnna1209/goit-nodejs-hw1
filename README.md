664c07807d6d65d117be4c5f3e66cb8c5df7bc06
PopovaAnna1209 <popovaanna120987@gmail.com>
2023-07-24 23:37:19 +0300

    commit-nodejs-1

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # goit-nodejs-hw1
+# Отримуємо і виводимо весь список контактів у вигляді таблиці (console.table)
+node index.js --action="list"
+https://monosnap.com/file/66jJSGgt8mcQXlagEmWHWXtasPZsoy
+
+# Отримуємо контакт по id і виводимо у консоль об'єкт контакта або null якщо контакту з таким id не існує
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+https://monosnap.com/file/0lQYSS4stGq6v2oFrk98o9JKXjlwRF
+
+# Додаємо контакт та виводимо в консоль об'єкт новоствореного контакту
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/afst1uXH3I06i9WKgPLhsoQfooSk3X
+
+# Видаляємо контакт та виводимо в консоль об'єкт видаленого контакту або null якщо контакту з таким id не існує
+node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/DIHgr0wjakZzQjblyEWEFtfFFAFRPw
---
+++ index.js
@@ -0,0 +1,38 @@
+const { program } = require("commander");
+const contacts = require("./contacts");
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      return console.table(contactsList);
+
+    case "get":
+      const getById = await contacts.getContactById(id);
+      return console.table(getById);
+
+    case "add":
+      const addContact = await contacts.addContact({ name, email, phone });
+      return console.table(addContact);
+
+    case "remove":
+        const removeContact = await contacts.removeContact(id);
+        return console.table(removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
